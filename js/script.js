cfc51c42671ba61991cbe81878cb9f27270359d3
Abdullah-Al-Tamim-573 <abdullahaltamim573@gmail.com>
2026-02-23 10:56:34 +0600

    add condition to the interview btn and if anyone click there create a object and give some details

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ let rejectedArry = [];
 // job tracker 2 card count number (Interview and Rejected)
 let interviewCountNumber = document.querySelector(".interview-count-number");
 let rejectedinterviewCountNumber = document.querySelector(
-  ".rejected-count-number",
+    ".rejected-count-number",
 );
 interviewCountNumber.innerText = interviewArry.length;
 rejectedinterviewCountNumber.innerText = rejectedArry.length;
@@ -38,31 +38,31 @@ interviewSection.style.display = "none";
 rejectedSection.style.display = "none";
 
 function changePageAndColorBtn(id) {
-  //    3 filter btn
-  let allBtn = document.getElementById("all-filter-btn");
-  let interviewBtn = document.getElementById("interview-filter-btn");
-  let rejectedBtn = document.getElementById("rejected-filter-btn");
+    //    3 filter btn
+    let allBtn = document.getElementById("all-filter-btn");
+    let interviewBtn = document.getElementById("interview-filter-btn");
+    let rejectedBtn = document.getElementById("rejected-filter-btn");
 
-  allBtn.classList.remove("btn-primary");
-  interviewBtn.classList.remove("btn-primary");
-  rejectedBtn.classList.remove("btn-primary");
+    allBtn.classList.remove("btn-primary");
+    interviewBtn.classList.remove("btn-primary");
+    rejectedBtn.classList.remove("btn-primary");
 
-  let changeColorBtn = document.getElementById(id);
-  changeColorBtn.classList.add("btn-primary");
+    let changeColorBtn = document.getElementById(id);
+    changeColorBtn.classList.add("btn-primary");
 
-  if (id === "all-filter-btn") {
-    jobCardsParent.style.display = "block";
-    interviewSection.style.display = "none";
-    rejectedSection.style.display = "none";
-  } else if (id === "interview-filter-btn") {
-    jobCardsParent.style.display = "none";
-    interviewSection.style.display = "block";
-    rejectedSection.style.display = "none";
-  } else {
-    jobCardsParent.style.display = "none";
-    interviewSection.style.display = "none";
-    rejectedSection.style.display = "block";
-  }
+    if (id === "all-filter-btn") {
+        jobCardsParent.style.display = "block";
+        interviewSection.style.display = "none";
+        rejectedSection.style.display = "none";
+    } else if (id === "interview-filter-btn") {
+        jobCardsParent.style.display = "none";
+        interviewSection.style.display = "block";
+        rejectedSection.style.display = "none";
+    } else {
+        jobCardsParent.style.display = "none";
+        interviewSection.style.display = "none";
+        rejectedSection.style.display = "block";
+    }
 }
 
 // filter btn logic end
@@ -70,31 +70,37 @@ function changePageAndColorBtn(id) {
 // add clcik handler in main parent
 
 document.querySelector("main").addEventListener("click", function (e) {
-  let jobCardTitle = document.querySelector(".job-card-title");
-  let jobCardSubtitle = document.querySelector(".job-card-subtitle");
-  let jobCardTypeAndSalary = document.querySelector(".job-card-type-and-salary",);
-  let notAppliedJobCardStatus = document.querySelector(".not-applied-job-card-status",);
-  let jobCardDescribtion = document.querySelector(".job-card-describtion");
-  let jobCardInterviewBtn = document.querySelector(".job-card-interview-btn");
-  let jobCardRejectedBtn = document.querySelector(".job-card-rejected-btn");
+    if (e.target.matches('.job-card-interview-btn')) {
+        let parentNode = e.target.parentNode.parentNode;
+        //    job card element details
+        let jobCardTitle = parentNode.querySelector(".job-card-title");
+        let jobCardSubtitle = parentNode.querySelector(".job-card-subtitle");
+        let jobCardTypeAndSalary = parentNode.querySelector(".job-card-type-and-salary",);
+        let notAppliedJobCardStatus = parentNode.querySelector(".not-applied-job-card-status",);
+        let jobCardDescribtion = parentNode.querySelector(".job-card-describtion");
+        let jobCardInterviewBtn = parentNode.querySelector(".job-card-interview-btn");
+        let jobCardRejectedBtn = parentNode.querySelector(".job-card-rejected-btn");
 
-//   text
-let jobCardTitleText = jobCardTitle.innerText;
-let jobCardSubtitleText = jobCardSubtitle.innerText;
-let jobCardTypeAndSalaryText = jobCardTypeAndSalary.innerText;
-let notAppliedJobCardStatusText = notAppliedJobCardStatus.innerText;
-let jobCardDescribtionText = jobCardDescribtion.innerText;
-let jobCardInterviewBtnText = jobCardInterviewBtn.innerText;
-let jobCardRejectedBtnText = jobCardRejectedBtn.innerText;
+        //   job card element text details
+        let jobCardTitleText = jobCardTitle.innerText;
+        let jobCardSubtitleText = jobCardSubtitle.innerText;
+        let jobCardTypeAndSalaryText = jobCardTypeAndSalary.innerText;
+        let notAppliedJobCardStatusText = notAppliedJobCardStatus.innerText;
+        let jobCardDescribtionText = jobCardDescribtion.innerText;
+        let jobCardInterviewBtnText = jobCardInterviewBtn.innerText;
+        let jobCardRejectedBtnText = jobCardRejectedBtn.innerText;
 
-  let obj = {
-    jobCardTitle,
-    jobCardSubtitle,
-    jobCardTypeAndSalary,
-    notAppliedJobCardStatus,
-    jobCardDescribtion,
-    jobCardInterviewBtn,
-    jobCardRejectedBtn
-  }
-  console.log(obj)
+        //  job card element text details covert to object
+        let jobCardDetailsObj = {
+            jobCardTitleText,
+            jobCardSubtitleText,
+            jobCardTypeAndSalaryText,
+            notAppliedJobCardStatusText,
+            jobCardDescribtionText,
+            jobCardInterviewBtnText,
+            jobCardRejectedBtnText
+        }
+        console.log(jobCardDetailsObj)
+    }
+
 });
